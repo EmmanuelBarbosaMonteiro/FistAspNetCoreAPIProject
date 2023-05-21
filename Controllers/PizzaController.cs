f8faf72196e7b1e2ca3ca034dae0e5cbe99259f8
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-05-21 06:48:24 -0300

    Implementado operações CRUD completas no PizzaControler

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -37,7 +37,45 @@ public class PizzaController : ControllerBase
         return pizza;    
     }
 
-    // POST action
+    // Action to handle POST requestes and create a new pizza.
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        // Return a CreatedAction status along with the location of new resource and the resource itself.
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+    }
 
-    // DELETE action
+    // Action to handle PUT requests and update an existing pizza.
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        // Check if the id in the path matches with the id of pizza in the request body.
+        if (id != pizza.Id)
+            return BadRequest();
+
+        // Update the pizza.
+        PizzaService.Update(pizza);
+
+        // Return a NoContent status indicating that the operation was successful.
+        return NoContent();    
+    }
+    
+    // Action to handle DELETE requests and delete an existing pizza.
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        // Get the pizza by id.
+        var pizza = PizzaService.Get(id);
+        
+        // If no pizza with such id is found, return a 404 Not Found status.
+        if (pizza is null)
+            return NotFound();
+
+        // Delete the pizza.
+        PizzaService.Delete(id);
+
+        // Return a NoContent status indicating that the operation was successful.
+        return NoContent();    
+    }
 }
