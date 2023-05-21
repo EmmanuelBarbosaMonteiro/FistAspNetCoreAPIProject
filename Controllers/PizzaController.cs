90a90929a24884c1e74f15ad56eeaf814c14e3fd
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-05-21 04:41:55 -0300

    Criado o esqueleto do controlador PizzaController

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,22 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiContoller]
+[Route("[contoller])"]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+    // GET all action
+
+    // GET vy Id action
+
+    // POST action
+
+    // DELETE action
+}
